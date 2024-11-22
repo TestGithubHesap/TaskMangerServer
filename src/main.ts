4654d4b1e1562df90cf649f84c2c154716a41d3a
MkMuhammetKaradag <mk.muhammetkaradag@gmail.com>
2024-11-22 14:06:27 +0300

    change main.ts  cors

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', '*'],
     allowedHeaders: [
       'Content-Type',
       'apollo-require-preflight',
